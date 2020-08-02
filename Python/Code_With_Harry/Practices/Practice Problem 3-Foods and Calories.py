9651c4c2d39d28cd7175cfb3b1cec3f8e92b227b
Aritra Roy <aritraroy24@gmail.com>
2020-08-02 11:23:20 +0530

    Up to Practice Problem 3

--- Python/Code_With_Harry/Practices/Practice Problem 3-Foods and Calories.py
+++ Python/Code_With_Harry/Practices/Practice Problem 3-Foods and Calories.py
@@ -17,30 +17,24 @@ try:
             # For taking each element from number_list and food_list according to their index number
             new_list.append(f"{food_list[number_list.index(i)]}-{number_list[number_list.index(i)]}")
         print(f"\nSorted list(from lower calorie to higher):\n{new_list}\n")
-        
-        
         #Reverse Process 1
         rev1_new_list = new_list[:]
         rev1_new_list.reverse()
         print(rev1_new_list)
-
         #Reverse Process 2
         rev2_new_list=new_list[::-1]
         print(rev2_new_list)
-
         #Reverse Process 3
         rev3_new_list = new_list[:]
         for i in range(len(rev3_new_list)//2):
             rev3_new_list[i], rev3_new_list[len(rev3_new_list)-i-1] = rev3_new_list[len(rev3_new_list)-i-1],rev3_new_list[i]
         print(rev3_new_list)
-        
         ans = (rev1_new_list==rev2_new_list and rev1_new_list==rev3_new_list)
         if ans==True:
             print("\nAll the three methods give same results")
         else:
-            print("\nYou haven't implemented the three methods correctly")
-            
+            print("\nYou haven't implemented the three methods correctly") 
     except ValueError:
         print("\nValueError: Calorie should be an integer and Food should be a string")
 except ValueError:
-    print("\nValueError: Range should be an integer")
+    print("\nValueError: Number of food should be an integer")
