c17e92ad62d8b1bb92ffb97abb0903b5e04e7e45
Aritra Roy <aritraroy24@gmail.com>
2020-08-28 17:09:23 +0530

    JavaScript Tutorial_3 added

---
+++ JavaScript/Tutorial_3/js/index.js
@@ -0,0 +1,50 @@
+// Variables in JavaScript
+// var, let, const
+
+
+// var
+var name = "Aritra";
+var subject;
+var marks = 7.33;
+marks = 70
+subject = "Chemistry";
+console.log(name, subject, marks);
+// Rules for JS variables
+/*
+1. Cannot start with numbers
+2. Can start with letter, numbers, _ or $
+3. Are case sensitive
+*/
+var city = "Delhi";
+console.log(city);
+
+
+// const
+const ownersName = "Aritra Roy";
+// ownersName = "Ari" (Can't do this)
+console.log(ownersName);
+const fruit = "Mango";
+const arr1 = [2, 20, 30, 40, 10];
+// arr1.push(100);
+console.log(arr1)
+
+
+// let
+{
+    let city = "Kolkata"; //let creates a local variable
+    city = "Mumbai"
+    console.log(city);
+}
+console.log(city);
+
+
+
+
+/*Most Common Programming Case Types:
+
+1. camelCase
+2. kebab-case
+3. snake_case
+4. PascalCase
+
+*/
