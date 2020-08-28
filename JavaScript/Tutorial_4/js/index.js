6daa91fe116d7cef6377fe7ae7a31a1496b47618
Aritra Roy <aritraroy24@gmail.com>
2020-08-28 17:36:23 +0530

    JavaScript Tutorial_4 added

---
+++ JavaScript/Tutorial_4/js/index.js
@@ -0,0 +1,53 @@
+// Primitive Data Types
+
+
+// String
+let name = "Aritra";
+console.log("My name is " + name);
+console.log("Data type  is " + (typeof name));
+
+// Numbers
+let marks = 80;
+console.log("Data type  is " + (typeof marks));
+
+// Boolean
+let isDriver = true;
+console.log("Data type  is " + (typeof isDriver));
+
+// Null
+let nullVar = null;
+console.log("Data type  is " + (typeof nullVar));
+
+// Undefined
+let unDef = undefined;
+console.log("Data type  is " + (typeof unDef));
+
+
+
+
+
+// Reference Data Types
+
+
+// Arrays
+myarr = [20, 30, 40, 50, false, "string"];
+console.log("Data type  is " + (typeof myarr));
+
+// Object Literals
+let stMarks = {
+    Aritra: 90,
+    Sujan: 85,
+    Chail: 70
+}
+console.log(stMarks);
+console.log("Data type  is " + (typeof stMarks));
+
+// Functions
+function findName() {
+    
+}
+console.log("Data type  is " + (typeof findName));
+
+// Dates
+let date = new Date();
+console.log("Data type  is " + (typeof date));
