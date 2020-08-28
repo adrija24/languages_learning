13768da2db4735ad7da65ead35e18faa09725c61
Aritra Roy <aritraroy24@gmail.com>
2020-08-28 16:36:46 +0530

    JavaScript Tutorial-2 added

---
+++ JavaScript/Tutorial 2/js/index.js
@@ -0,0 +1,21 @@
+console.time("Your code took");
+console.log("Hello World!!!");
+console.log(10+20);
+console.log(15);
+console.log(true);
+console.log([10,20,25,85,60]);
+console.log({Aritra:"this", marks:50});
+console.table({Aritra:"this", marks:50});
+console.warn("This is a warning!!!");
+// console.clear
+console.timeEnd("Your code took");
+// console.assert(526<150, "Age>150 is not possible")
+console.error("This is an error")
+
+/*
+This 
+is 
+a 
+multi-line 
+comment
+*/
