b610fe406bcd5d1ab93ddf2c487a977a0080ce68
Aritra Roy <aritraroy24@gmail.com>
2020-08-30 09:49:56 +0530

    JavaScript Tutorial_11 added

---
+++ JavaScript/Tutorial_11/js/index.js
@@ -0,0 +1,12 @@
+let a = window.document;
+alert("Hello Aritra!!!")
+a = prompt("This will destroy your computer!!! Type yor name-")
+a = confirm("Do you want to delete this page?")
+a = window.innerHeight;
+a = window.innerWidth;
+a = scrollX
+a =scrollY
+a = location
+a = location.toString
+a = window.history
+console.log(a)
