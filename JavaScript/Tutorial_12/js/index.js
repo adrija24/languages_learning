366bd93c0a338fb8f95efdb14d13f29c70ce647d
Aritra Roy <aritraroy24@gmail.com>
2020-08-30 10:10:45 +0530

    JavaScript Tutorial_12 added

---
+++ JavaScript/Tutorial_12/js/index.js
@@ -0,0 +1,9 @@
+let a = document;
+a = document.all;
+a = document.body;
+a = document.forms;
+Array.from(a).forEach(function(element){
+    console.log(element)
+})
+a = document.links[0];
+console.log(a);
