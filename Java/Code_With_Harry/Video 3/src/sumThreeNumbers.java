df60225f2904e192596675a5f89d911ef658087f
Aritra Roy <aritraroy24@gmail.com>
2020-09-02 21:58:58 +0530

    Java Video 3 added

---
+++ Java/Code_With_Harry/Video 3/src/sumThreeNumbers.java
@@ -0,0 +1,10 @@
+public class sumThreeNumbers {
+
+	public static void main(String[] args) {
+		int num1 = 10; int num2 = 15; int num3 = 25;
+		int sum = num1+num2+num3;
+		System.out.println("The sum of three number is : "+sum);
+
+	}
+
+}
