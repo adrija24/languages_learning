0fcbad81900d035ef8c7141fbc64856f6709d9df
Aritra Roy <aritraroy24@gmail.com>
2020-09-02 08:28:29 +0530

    Java video 2 (Structure of Java Program) added

---
+++ Java/Code_With_Harry/Video 2/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello World!!!");
+	}
+}
+
+//Naming Convention:
+//	Class - PascalConvention (First letter capital)
+//		example: AddTwoNumbers --> Pascal naming convention
+//	Function - camelCaseConvension (First letter small)
+//		example: addTwoNumbers --> Camel naming convention
