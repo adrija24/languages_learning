7884bd961d1339d8ad79c89f403b712408121a9a
Aritra Roy <aritraroy24@gmail.com>
2020-09-04 10:41:29 +0530

    Java Video 3 on literals added

---
+++ Java/Code_With_Harry/Video 4/src/Literals.java
@@ -0,0 +1,23 @@
+public class Literals {
+
+	public static void main(String[] args) {
+		byte age = 23;
+		int age2 = 56;
+		short age3 = 80;
+		long ln = 6544326781l;
+		char ch = 'A';
+		float f1 = 5.6f;
+		double d1 = 4.55;
+		boolean a = true;
+		String str = "Aritra";
+		System.out.println(age);
+		System.out.println(age2);
+		System.out.println(age3);
+		System.out.println(ch);
+		System.out.println(f1);
+		System.out.println(d1);
+		System.out.println(ln);
+		System.out.println(a);
+		System.out.print(str);
+	}
+}
