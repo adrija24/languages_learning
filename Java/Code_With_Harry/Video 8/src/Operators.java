6b2a576a15542babc71ca0d1af5f7f6903124ced
Aritra Roy <aritraroy24@gmail.com>
2020-09-07 18:52:06 +0530

    Java Video 8 (operators) added

---
+++ Java/Code_With_Harry/Video 8/src/Operators.java
@@ -0,0 +1,22 @@
+public class Operators {
+
+	public static void main(String[] args) {
+		// Arithmetic operator
+		int a = 4;
+		int b = 10%a; 
+		System.out.println(b); // Module operator
+		int c = 9;
+		c += 3;
+		System.out.println(c);
+		
+		 // Comparison operator
+		System.out.println(6==8);
+		
+		// Logical operator
+		System.out.println(65>10 && 6==6);
+		System.out.println(65<10 || 6==8);
+		
+		// Bitwise operator
+		System.out.println(2&3);
+	}
+}
